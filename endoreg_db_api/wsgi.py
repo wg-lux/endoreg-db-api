6f4a60215a4a11ceccd9bbb0cac7f06a46c5a580
Thomas <tlux14@gmail.com>
2024-11-19 15:25:32 +0100

    fix settings defaults

--- endoreg_db_api/wsgi.py
+++ endoreg_db_api/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'endoreg_db_api.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'endoreg_db_api.settings_prod')
 
 application = get_wsgi_application()
