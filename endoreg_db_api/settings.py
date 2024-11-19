a9ae63b6aade321f141cc553c5959990bb0beeb7
Thomas <tlux14@gmail.com>
2024-11-19 13:05:39 +0100

    awsd

--- endoreg_db_api/settings.py
+++ endoreg_db_api/settings.py
@@ -94,18 +94,18 @@ WSGI_APPLICATION = 'endoreg_db_api.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-    "default": {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'aglnet_base',
-        'USER': DB_USER,
-        'PASSWORD': DB_PWD,  # Use the loaded password
-        'HOST': '172.16.255.1',  # Set to 'localhost' if the server is local
-        'PORT': '5432',       # Default PostgreSQL port
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+    # "default": {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'aglnet_base',
+    #     'USER': DB_USER,
+    #     'PASSWORD': DB_PWD,  # Use the loaded password
+    #     'HOST': '172.16.255.1',  # Set to 'localhost' if the server is local
+    #     'PORT': '5432',       # Default PostgreSQL port
+    # }
 }
 
 
