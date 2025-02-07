e6f2a31f30826942d82a196434a0968c64043cf9
maxhild <maxhild10@gmail.com>
2025-02-07 15:09:07 +0100

    Django api sketch

---
+++ endoreg_db_api/api/views.py
@@ -0,0 +1,44 @@
+from rest_framework import viewsets
+from ..endoreg_db-production-endoreg-db.models import Intervention
+
+from ..endoreg_db-production-endoreg-db.serializers import InterventionSerializer
+
+class InterventionViewSet(viewsets.ModelViewSet):
+    queryset = Intervention.objects.all()
+    serializer_class = InterventionSerializer
+    
+class CenterViewSet(viewsets.ModelViewSet):
+    queryset = Center.objects.all()
+    serializer_class = CenterSerializer
+
+class ExaminationViewSet(viewsets.ModelViewSet):
+    queryset = Examination.objects.all()
+    serializer_class = ExaminationSerializer
+    
+class FindingViewSet(viewsets.ModelViewSet):
+    queryset = Finding.objects.all()
+    serializer_class = FindingSerializer
+    
+class LocationClassificationViewSet(viewsets.ModelViewSet):
+    queryset = LocationClassification.objects.all()
+    serializer_class = LocationClassificationSerializer
+    
+class LocationChoiceViewSet(viewsets.ModelViewSet):
+    queryset = LocationChoice.objects.all()
+    serializer_class = LocationChoiceSerializer
+
+class MorphologyClassificationViewSet(viewsets.ModelViewSet):
+    queryset = MorphologyClassification.objects.all()
+    serializer_class = MorphologyClassificationSerializer
+
+class MorphologyChoiceViewSet(viewsets.ModelViewSet):
+    queryset = MorphologyChoice.objects.all()
+    serializer_class = MorphologyChoiceSerializer
+
+class InterventionViewSet(viewsets.ModelViewSet):
+    queryset = Intervention.objects.all()
+    serializer_class = InterventionSerializer
+    
+class PatientDataViewSet(viewsets.ModelViewSet):
+    queryset = PatientData.objects.all()
+    serializer_class = PatientDataSerializer
